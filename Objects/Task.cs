569aa217f3242c419b47859eccde21f93529554b
talayruk <notejde@gmail.com>
2016-09-19 11:03:38 -0700

    add Database TodoList Day1

---
+++ Objects/Task.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace ToDoList
+{
+  public class Task
+  {
+    private int _id;
+    private string _description;
+
+    public Task()
+  }
+}
